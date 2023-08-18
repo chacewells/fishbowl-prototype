ba6d17b70ec85c594e3f5ebf98fc11dfb135d170
chace <chacewells@gmail.com>
2023-08-18 06:47:46 -0700

    Add some content to prizes

--- src/fishbowl/prizes.js
+++ src/fishbowl/prizes.js
@@ -1,10 +1,41 @@
 import {useParams} from "react-router-dom";
+import {useRef, useState} from "react";
+
+const initialPrizes = [
+  {
+    id: 1,
+    name: "A brand new car!"
+  },
+  {
+    id: 2,
+    name: "This leftover pizza"
+  },
+];
 
 export default function PrizesPage() {
-  let {fishbowlId} = useParams();
+  const {fishbowlId} = useParams();
+  const [nextId, setNextId] = useState(3);
+  const [prizes, setPrizes] = useState(initialPrizes);
+  const prizeInputRef = useRef();
+
+  function handleAdd() {
+    const newPrize = {
+      id: nextId,
+      name: prizeInputRef.current.value,
+    };
+    prizeInputRef.current.value = "";
+    setPrizes([...prizes, newPrize]);
+
+    setNextId(1+nextId);
+  }
+
   return (
     <>
       <h1>Fishbowl {fishbowlId} Prizes</h1>
+      <ul>
+        {prizes.map((p) => <li key={p.id}>{p.name}</li>)}
+      </ul>
+      <input type="text" ref={prizeInputRef}/> <button onClick={handleAdd}>+ Add</button>
     </>
   )
 }
