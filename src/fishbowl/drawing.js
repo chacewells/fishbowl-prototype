4987e079a2baeadaee627fd5c609b39f95f51578
chace <chacewells@gmail.com>
2023-08-20 14:42:49 -0700

    Style drawings page; closes #6

--- src/fishbowl/drawing.js
+++ src/fishbowl/drawing.js
@@ -1,18 +1,35 @@
 import {useParams} from "react-router-dom";
 
 export default function Drawing() {
+  const prizeAndWinners = [
+    {
+      prizeLabel: "Prize 1",
+      prizeWinner: "Sundar Pichai"
+    },
+    {
+      prizeLabel: "Prize 2",
+      prizeWinner: "Elon Musk"
+    }
+  ];
+
   let {fishbowlId} = useParams();
   return (
     <>
       <h1>Fishbowl {fishbowlId} Drawing</h1>
-      <ul>
-        <li>
-          Prize 1: <button>Draw</button> - Winner: Sundar Pichai
-        </li>
-        <li>
-          Prize 2: <button>Draw</button> - Winner: Elon Musk
-        </li>
-      </ul>
+      {prizeAndWinners.map((prize) => (
+        <div className="card" style={{marginBottom: "0.5em"}}>
+          <div className="card-header">
+            <div className="card-title">
+              {prize.prizeLabel}
+              <button className="btn btn-primary float-end">Draw</button>
+            </div>
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">Winner</h5>
+            <p className="card-text">{prize.prizeWinner}</p>
+          </div>
+        </div>
+      ))}
     </>
   )
 }
